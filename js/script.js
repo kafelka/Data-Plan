0b5b5787b9389aec5adfc0027e839c9a99ab51cf
Magdalena Kafel <na.hasiok@gmail.com>
2018-11-21 21:46:43 +0000

    Completed js for chatterbox, modified classes in html

--- js/script.js
+++ js/script.js
@@ -1,32 +1,75 @@
-var priceSelected;
-var total;
-var planFrequency;
+var priceGB = 0;
+var total = 0;
+var planFrequency = 1;
+var priceMinutes = 0;
 
-$(".my-activity").click(function(event) {
-  $(".my-activity:checked").each(function() {
-    priceSelected = parseInt($(this).val());
-    $("#amount").val(priceSelected);
-  })
+
+function calculate() {
+  total = (priceGB + priceMinutes) * planFrequency;
+  $("#amount").val(total);
+}
+
+$(".my-giga").click(function(event) {
+  $(".my-giga:checked").each(function() {
+    priceGB = parseInt($(this).val());
+    // $("#amount").val(priceGB);
+  });
+  calculate();
 });
 
 $(".my-frequency").click(function(e){
   $(".my-frequency:checked").each(function(){
-    planFrequency = $(this).val();
+    planFrequency = parseInt($(this).val());
   });
-
-  if(planFrequency === "monthly") {
-    total = priceSelected;
-  } else {
-    total = priceSelected * 12;
-  }
-  $("#amount").val(total);
+  calculate();
+  // if(planFrequency === "monthly") {
+  //   total = priceGB;
+  // } else {
+  //   total = priceGB * 12;
+  // }
+  // $("#amount").val(total);
 });
 
+$(".my-minutes").click(function(e){
+  $(".my-minutes:checked").each(function(e){
+    // priceMinutes = $(this).val();
+    priceMinutes = parseInt($(this).data('price'));
+  });
+  calculate();
+
+  });
+
+  // if ((planFrequency === "monthly") && (priceMinutes === "180min")) {
+  //   total = priceGB + priceMinutesPrice;
+  // } else if ((planFrequency === "monthly") && (priceMinutes === "300min")) {
+  //   total = priceGB + priceMinutesPrice;
+  // } else if ((planFrequency === "monthly") && (priceMinutes === "600min")) {
+  //   total = priceGB + priceMinutesPrice;
+  // } else if ((planFrequency === "monthly") && (priceMinutes === "unlimited")) {
+  //   total = priceGB + priceMinutesPrice;
+  // } else if ((planFrequency === "annually") && (priceMinutes === "180min")) {
+  //   total = ((priceGB + priceMinutesPrice) * 12);
+  // } else if ((planFrequency === "annually") && (priceMinutes === "300min")) {
+  //   total = ((priceGB + priceMinutesPrice) * 12);
+  // } else if ((planFrequency === "annually") && (priceMinutes === "600min")) {
+  //   total = ((priceGB + priceMinutesPrice) * 12);
+  // } else {
+  //   total = ((priceGB + priceMinutesPrice) * 12);
+  // }
+
+
+// $("#amount").val(total);
+
+
 $(".btn-success").click(function(e) {
-    $("#amount").val("");
-})
+  priceGB = 0;
+  total = 0;
+  planFrequency = 1;
+  priceMinutes = 0;
 
+  $("#amount").val("");
+  $("input[type=radio]:checked").each(function(e){
+    $(this).prop("checked", false);
+  })
 
-
-
-// parseInt($(this).val());
+});
